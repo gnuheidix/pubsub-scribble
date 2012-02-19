6c35126e50bbd9ea0744e84d6f03e196a88dcf56
Thomas Heidrich <heidi@miradlo.de>
2012-02-19 20:51:56 +0100

    Basisstyle grob nach CI, Farbwähler

--- move.js
+++ move.js
@@ -1,30 +1,26 @@
 var pubsub = pubsubio.connect('gnuheidix.de:8080');
 var canvas = document.getElementById('spielwiese');
+var picker = document.getElementById('farbe');
+var header = document.getElementById('oben');
+var footer = document.getElementById('unten');
 var pubsubChannel = 'move';
 var mouse = false;
-var windowW;
-var windowH;
 
 var resize = function(){
-    windowW = window.innerWidth;
-    windowH = window.innerHeight;
+    var windowW = window.innerWidth;
+    var windowH = window.innerHeight;
+    var headerH = header.offsetHeight;
+    var footerH = footer.offsetHeight;
     canvas.setAttribute('width', windowW);
-    canvas.setAttribute('height', windowH);
+    canvas.setAttribute('height', windowH - footerH - headerH - 50);
 }
-window.onresize = resize;
-
-pubsub.subscribe({name:pubsubChannel}, function(msg){
-    if(canvas.getContext){
-        var ctx = canvas.getContext('2d');
-        ctx.fillRect(msg.x, msg.y, -10, -10);
-    }
-});
 
 var getConfig = function(mouseX, mouseY){
     return {
         name: pubsubChannel
-        , x: (mouseX - canvas.offsetLeft)
-        , y: (mouseY - canvas.offsetTop)
+        , x: mouseX - canvas.offsetLeft
+        , y: mouseY - canvas.offsetTop
+        , c: picker.value
     };
 };
 
@@ -43,4 +39,14 @@ document.addEventListener('mousedown', function(evt){
     pubsub.publish(getConfig(evt.clientX, evt.clientY));
 });
 
+window.onresize = resize;
+
 resize();
+
+pubsub.subscribe({name:pubsubChannel}, function(msg){
+    if(canvas.getContext){
+        var ctx = canvas.getContext('2d');
+	ctx.fillStyle = '#' + msg.c;
+        ctx.fillRect(msg.x, msg.y, -10, -10);
+    }
+});
