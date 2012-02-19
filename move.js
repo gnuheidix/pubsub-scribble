666fdc10f5784ba8051808b8dccdf348adb2b5e7
Thomas Heidrich <heidi@miradlo.de>
2012-02-19 08:55:38 +0100

    MouseDown auch mit Publish versehen, Code bissel aufgeräumt

--- move.js
+++ move.js
@@ -1,8 +1,9 @@
 var pubsub = pubsubio.connect('gnuheidix.de:8080');
+var canvas = document.getElementById('spielwiese');
+var pubsubChannel = 'move';
 var mouse = false;
 var windowW;
 var windowH;
-var canvas = document.getElementById('spielwiese');
 
 var resize = function(){
     windowW = window.innerWidth;
@@ -10,10 +11,9 @@ var resize = function(){
     canvas.setAttribute('width', windowW);
     canvas.setAttribute('height', windowH);
 }
-resize();
 window.onresize = resize;
 
-pubsub.subscribe({name:'move'}, function(msg){
+pubsub.subscribe({name:pubsubChannel}, function(msg){
     if(canvas.getContext){
         var ctx = canvas.getContext('2d');
         ctx.fillRect(msg.x, msg.y, -10, -10);
@@ -22,14 +22,25 @@ pubsub.subscribe({name:'move'}, function(msg){
 
 document.addEventListener('mousemove',function(evt){
     if(mouse){
-        var config = {name: 'move'
+        var config = {name: pubsubChannel
                         , x: evt.clientX
                         , y: evt.clientY
-                        , m: mouse
         };
         pubsub.publish(config);
     }
 });
 
-document.addEventListener('mouseup', function(){ mouse = false; });
-document.addEventListener('mousedown', function(){ mouse = true; });
+document.addEventListener('mouseup', function(evt){
+    mouse = false;
+});
+
+document.addEventListener('mousedown', function(evt){
+    mouse = true;
+    var config = {name: pubsubChannel
+                    , x: evt.clientX
+                    , y: evt.clientY
+    };
+    pubsub.publish(config);
+});
+
+resize();
