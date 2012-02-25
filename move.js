dff266ab4de55655f45f1f140ebff39d1c1d50cf
Thomas Heidrich <heidi@miradlo.de>
2012-02-25 18:50:35 +0100

    Adressen und Port auf gnuheidix:8080 ausgerichtet

--- move.js
+++ move.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://127.0.0.1:3001');
+var socket = io.connect('http://scribble.gnuheidix.de:8080');
 
 var canvas = document.getElementById('spielwiese');
 var picker = document.getElementById('farbe');
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(3001);
+var io = require('socket.io').listen(8080);
 
 io.sockets.on('connection', function(socket) {
 	socket.on('move', function(pos) {
