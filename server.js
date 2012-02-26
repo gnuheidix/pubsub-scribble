a5f79e92d185ec06f72b2c3de691d83f0b609594
Thomas Heidrich <heidi@miradlo.de>
2012-02-26 08:07:54 +0100

    Config gesetzt (u.a. LogLevel)

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var io = require('socket.io').listen(8080);
 
+io.configure(function(){
+	io.enable('browser client minification');  // send minified client
+	io.enable('browser client etag');          // apply etag caching logic based on version number
+	io.enable('browser client gzip');          // gzip the file
+	io.set('log level', 1);                    // reduce logging
+	io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+	    'websocket'
+	  , 'flashsocket'
+	  , 'htmlfile'
+	  , 'xhr-polling'
+ 	  , 'jsonp-polling'
+	]);
+});
+
 io.sockets.on('connection', function(socket) {
 	socket.on('move', function(pos) {
 		io.sockets.emit('moved', pos);
